15ae1f4a792b6019cde2d9c2f72021d0043883c3
Jmbac0n <jakematthias@hotmail.com>
2021-01-08 22:06:40 +0000

    speech_rec_update_002

--- Speech_Rec.py
+++ Speech_Rec.py
@@ -1,22 +1,24 @@
-import pyaudio, os
+import pyaudio, os, pyttsx3
 import speech_recognition as sr
 import pocketsphinx
 
 r = sr.Recognizer()
 m = sr.Microphone(device_index=0)
+engine = pyttsx3.init()
 
-#with m as source:
-    #print('Speak')
-    #print(m.list_microphone_names())
-    #audio = r.listen(source)
-                                                                       
+# Listen to audio and save as audio file
+with m as source:
+    print('Speak')
+    audio = r.listen(source)
 
-#for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    #print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+with open("microphone-results.wav", "wb") as f:
+    f.write(audio.get_wav_data())
 
+# Uses audio file saved in same file as script                                                                       
 from os import path
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "hello.wav")
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "microphone-results.wav")
 
+# Uses PocketSphinx to transcribe
 r = sr.Recognizer()
 with sr.AudioFile(AUDIO_FILE) as source:
     audio = r.record(source)
@@ -28,8 +30,11 @@ except sr.UnknownValueError:
 except sr.RequestError as e:
     print("Error; {0}".format(e))
 
+# Save transcribed audio to string and use if to determine response
 text_string = r.recognize_sphinx(audio)
-if text_string == "hello":
-    print("How are you?")
+if text_string == "shut down":
+    engine.say("Okay shutting down")
+    engine.runAndWait()
 
-# Have it respond with text to speech 
+#for index, name in enumerate(sr.Microphone.list_microphone_names()):
+    #print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
